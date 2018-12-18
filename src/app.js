aa016cc2e164e708ed11a5570950759d09a0c139
hehe1111 <2908749709@qq.com>
2018-12-18 23:00:05 +0800

    refactor: 引入 parcel；将 f-button.js 改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import FButton from './f-button.vue'
+
+Vue.component('FButton', FButton)
+
+new Vue({
+  el: '#app',
+  // components: {
+  //   FButton,
+  // }
+})
---
+++ src/f-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="f-button">按钮</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+.f-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding:0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background-color: var(--button-bg-color);
+
+  &:hover {
+    border-color: var(--border-hover-color);
+  }
+  &:active {
+    background-color: var(--button-active-bg-color);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
