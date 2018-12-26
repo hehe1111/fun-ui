b547d890af7d3517a4395bacc397fb2e148439b3
hehe1111 <2908749709@qq.com>
2018-12-26 23:03:50 +0800

    feat(Icon.vue): 添加 loading 图标及其动画

--- src/Icon.vue
+++ src/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="icon" aria-hidden="true">
+  <svg class="icon" :class="name === 'loading' ? 'loading' : ''" aria-hidden="true">
     <use :xlink:href="`#icon-${name}`"></use>
   </svg>
 </template>
@@ -23,5 +23,12 @@ export default {
     vertical-align: -0.15em;
     fill: currentColor;
     overflow: hidden;
+  }
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+  .loading {
+    animation: spin 1s infinite linear;
   }
 </style>
