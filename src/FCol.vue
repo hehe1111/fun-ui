d9cd44d7ae9ae4952acc1112a4bace111152c312
hehe1111 <2908749709@qq.com>
2019-06-03 20:49:25 +0800

    feat: 实现响应式

--- src/FCol.vue
+++ src/FCol.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script>
+let validator = paramObj => {
+  let keys = Object.keys(paramObj)
+  keys.forEach(k => {
+    if (!['span', 'offset'].includes(k)) {
+      return false
+    }
+  })
+  return true
+}
+
 export default {
   props: {
     span: {
@@ -15,6 +25,10 @@ export default {
       type: [Number, String],
       default: 0,
     },
+    ipad: { type: Object, validator, },
+    small_pc: { type: Object, validator, },
+    pc: { type: Object, validator, },
+    large_pc: { type: Object, validator, },
   },
   data() {
     return {
@@ -23,10 +37,13 @@ export default {
   },
   computed: {
     returnColClass() {
-      const { span, offset } = this;
+      const { span, offset, ipad, small_pc, pc, large_pc, createColClass } = this;
       return [
-        span && `col-${span}`,
-        offset && `col-offset-${offset}`
+        ...createColClass({ span, offset }),
+        ...createColClass(ipad, 'ipad-'),
+        ...createColClass(small_pc, 'small-pc-'),
+        ...createColClass(pc, 'pc-'),
+        ...createColClass(large_pc, 'large-pc-'),
       ]
     },
     returnGutterStyle() {
@@ -36,11 +53,24 @@ export default {
       }
     },
   },
+  methods: {
+    createColClass(obj, deviceType='') {
+      if (!obj) []
+      const { span, offset } = obj
+      return [
+        span && `col-${deviceType}${span}`,
+        offset && `col-offset-${deviceType}${offset}`,
+      ]
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
   .col {
+    // https://ant.design/components/grid-cn/#Col
+    // 0 - 576 - 768 - 992 - 1200
+    // 手机
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
@@ -52,6 +82,70 @@ export default {
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+    // 平板
+    @media (min-width: 577px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: col-offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    // 小屏幕电脑
+    @media (min-width: 769px) {
+      $class-prefix: col-small-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: col-offset-small-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    // 电脑
+    @media (min-width: 993px) {
+      $class-prefix: col-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: col-offset-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    // 大屏幕电脑及超大屏幕
+    @media (min-width: 1201px) {
+      $class-prefix: col-large-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $class-prefix: col-offset-large-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
