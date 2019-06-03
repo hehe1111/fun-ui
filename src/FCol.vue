beec584f620af037dd4215f822858e71b5a682de
hehe1111 <2908749709@qq.com>
2019-06-03 18:40:12 +0800

    feat: 添加 offset

--- src/FCol.vue
+++ src/FCol.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`]">
+  <div class="col" :class="[span && `col-${span}`, offset && `col-offset-${offset}`]">
     <slot></slot>
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    span: {
+      type: [Number, String],
+      default: 1,
+    },
+    offset: {
+      type: [Number, String],
+    },
+    default: 0,
+  },
+}
+</script>
+
 <style lang="scss" scoped>
   .col {
-    width: 100%;
     min-height: 100px;
     border: 1px solid green;
 
@@ -16,15 +30,12 @@
         width: ($n / 24) * 100%;
       }
     }
+
+    $class-prefix: col-offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
   }
 </style>
-
-<script>
-export default {
-  props: {
-    span: {
-      type: [Number, String],
-    }
-  },
-}
-</script>
