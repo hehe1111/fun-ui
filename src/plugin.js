73e6e607cc7d1353bd4f59cc7473de6b3c0e0b75
hehe1111 <2908749709@qq.com>
2019-06-16 14:12:08 +0800

    feat: 无论何时页面中都只有一个 toast 出现

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,22 @@
 import FToast from './FToast.vue';
 
+function createToast({ Vue, message, propsData }) {
+  const Constructor = Vue.extend(FToast);
+  const toast = new Constructor({ propsData });
+  toast.$slots.default = message;
+  toast.$mount();
+  document.body.appendChild(toast.$el);
+  return toast;
+}
+
+let currentToast;
+
 export default {
   install(Vue, options) {
     // eslint-disable-next-line prettier/prettier
     Vue.prototype.$toast = function (message, toastOptions) {
-      const Constructor = Vue.extend(FToast);
-      const vm = new Constructor({
-        propsData: toastOptions,
-      });
-      vm.$slots.default = message;
-      vm.$mount();
-      document.body.appendChild(vm.$el);
+      if (currentToast) currentToast.close();
+      currentToast = createToast({ Vue, message, propsData: toastOptions });
     };
   },
 };
