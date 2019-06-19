9c6ca4ca1fedbb982d34bbd552ae6cc4acb03de1
hehe1111 <2908749709@qq.com>
2019-06-19 18:38:19 +0800

    feat: 将content追加到body上以防止overflow: hidden；收拢关闭入口为close函数

--- src/FPopover.vue
+++ src/FPopover.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="popver">
-    <div class="content-container" v-if="visiable">
+  <div class="popver" @click="togglePopover" ref="popover">
+    <div class="content-container" v-if="visiable" ref="contentContainer">
       <slot name="content"></slot>
     </div>
-    <div class="button-container" @click="togglePopover">
+    <div class="button-container" ref="buttonContainer">
       <slot></slot>
     </div>
   </div>
@@ -18,28 +18,47 @@ export default {
     };
   },
   methods: {
-    togglePopover() {
-      this.visiable = !this.visiable;
+    relocatePopoverContent() {
+      document.body.appendChild(this.$refs.contentContainer);
+      const { top, left } = this.$refs.buttonContainer.getBoundingClientRect();
+      this.$refs.contentContainer.style.top = `${window.scrollY + top}px`;
+      this.$refs.contentContainer.style.left = `${window.scrollX + left}px`;
+    },
+    open() {
+      this.visiable = true;
       this.$nextTick(() => {
-        const eventHandler = () => {
-          this.visiable = false;
-          document.removeEventListener('click', eventHandler);
-        };
-        document.addEventListener('click', eventHandler);
+        this.relocatePopoverContent();
+        document.addEventListener('click', this.onClickDocument);
       });
+    },
+    close() {
+      this.visiable = false;
+      document.removeEventListener('click', this.onClickDocument);
+    },
+    onClickDocument(event) {
+      // 点击 popover 的内容部分时，内容部分不消失
+      if (this.visiable && this.$refs.contentContainer.contains(event.target)) {
+        return;
+      }
+      this.close();
+    },
+    togglePopover(event) {
+      this.visiable ? this.close() : this.open();
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+body {
+  position: relative;
+}
 .popver {
   display: inline-block;
-  position: relative;
-  > .content-container {
-    position: absolute;
-    bottom: 100%;
-    border: 1px solid red;
-  }
+}
+.content-container {
+  position: absolute;
+  border: 1px solid red;
+  transform: translateY(-100%);
 }
 </style>
