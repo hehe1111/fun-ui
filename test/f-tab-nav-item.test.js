d095fddc74fdee23384713f6744fe287e4b7d076
hehe1111 <2908749709@qq.com>
2019-06-21 20:00:02 +0800

    test: 修改 FTabNavItem 组件的测试用例以修复 eventBus 的注入错误

--- test/f-tab-nav-item.test.js
+++ test/f-tab-nav-item.test.js
@@ -15,7 +15,6 @@ Vue.component('f-tab-body-item', FTabBodyItem);
 
 // eslint-disable-next-line no-undef
 const { expect } = chai;
-const Constructor = Vue.extend(FTabNavItem);
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
@@ -23,32 +22,111 @@ Vue.config.devtools = false;
 describe('FTabNavItem', () => {
   it('存在', () => expect(FTabNavItem).to.exist);
 
-  it('可以接受 name 属性', () => {
-    const vm = new Constructor({
-      propsData: {
-        name: 'hahaha',
-      },
+  it('可以接受 name 属性', done => {
+    /**
+     * ERROR: '[Vue warn]: Injection "eventBus" not found
+     */
+    // const Constructor = Vue.extend(FTabNavItem);
+    // const vm = new Constructor({
+    //   propsData: {
+    //     name: 'hahaha',
+    //   },
+    // });
+    // vm.$mount();
+    // expect(vm.$el.getAttribute('data-name')).to.eq('hahaha');
+    // vm.$el.remove();
+    // vm.$destroy();
+
+    /**
+     * 故改用下面的测试用例
+     */
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    div.innerHTML = `
+      <f-tab selected="hahaha">
+        <f-tab-nav>
+          <f-tab-nav-item name="hahaha">hahaha</f-tab-nav-item>
+        </f-tab-nav>
+        <f-tab-body>
+          <f-tab-body-item name="hahaha">hahaha</f-tab-body-item>
+        </f-tab-body>
+      </f-tab>
+    `;
+
+    const vm = new Vue({
+      el: div,
     });
-    vm.$mount();
-    expect(vm.$el.getAttribute('data-name')).to.eq('hahaha');
-    vm.$el.remove();
-    vm.$destroy();
+
+    expect(
+      vm.$el.querySelector('.tab-nav-item').getAttribute('data-name')
+    ).to.eq('hahaha');
+    vm.$nextTick(() => {
+      // 不放到 $nextTick 则会报错
+      // ERROR: '[Vue warn]: Error in nextTick: "TypeError: Cannot read property 'style' of undefined"
+      // 报错来源 FTabNav.vue 组件
+      vm.$el.remove();
+      vm.$destroy();
+      done();
+    });
   });
 
-  it('可以接受 disabled 属性', () => {
-    const vm = new Constructor({
-      propsData: {
-        name: 'hahaha',
-        disabled: true,
-      },
-    });
-    vm.$mount();
-    expect(vm.$el.classList.contains('disabled')).to.eq(true);
+  it('可以接受 disabled 属性', done => {
+    /**
+     * ERROR: '[Vue warn]: Injection "eventBus" not found
+     */
+    // const Constructor = Vue.extend(FTabNavItem);
+    // const vm = new Constructor({
+    //   propsData: {
+    //     name: 'hahaha',
+    //     disabled: true,
+    //   },
+    // });
+    // vm.$mount();
+    // expect(vm.$el.classList.contains('disabled')).to.eq(true);
+    // const callback = sinon.fake();
+    // vm.$on('click', callback);
+    // vm.$el.click();
+    // expect(callback).to.have.not.been.called;
+    // vm.$el.remove();
+    // vm.$destroy();
+
+    /**
+     * 故改用下面的测试用例
+     */
+    const div = document.createElement('div');
+    document.body.appendChild(div);
     const callback = sinon.fake();
-    vm.$on('click', callback);
-    vm.$el.click();
+    div.innerHTML = `
+      <f-tab selected="hohoho">
+        <f-tab-nav>
+          <f-tab-nav-item name="hahaha" disabled @click="callback" class="target">hahaha</f-tab-nav-item>
+          <f-tab-nav-item name="hohoho">hohoho</f-tab-nav-item>
+        </f-tab-nav>
+        <f-tab-body>
+          <f-tab-body-item name="hahaha">hahaha</f-tab-body-item>
+          <f-tab-body-item name="hohoho">hohoho</f-tab-body-item>
+        </f-tab-body>
+      </f-tab>
+    `;
+
+    const vm = new Vue({
+      el: div,
+      methods: { callback },
+    });
+
+    const target = vm.$el.querySelector('.tab-nav-item.target');
+    vm.$nextTick(() => {
+      expect(target.classList.contains('disabled')).to.eq(true);
+    });
+    target.click();
     expect(callback).to.have.not.been.called;
-    vm.$el.remove();
-    vm.$destroy();
+    vm.$nextTick(() => {
+      // 不放到 $nextTick 则会报错
+      // ERROR: '[Vue warn]: Error in nextTick: "TypeError: Cannot read property 'style' of undefined"
+      // 报错来源 FTabNav.vue 组件
+      vm.$el.remove();
+      vm.$destroy();
+      done();
+    });
   });
 });
