a4c6327b1cde4a2b90e655f85b1a5d91eca66a74
hehe1111 <2908749709@qq.com>
2019-06-26 00:11:13 +0800

    feat: 使用 Vuepress 做 Fun UI 官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  // 网站的标题，它将会被用作所有页面标题的前缀，同时，默认主题下，它将显示在导航栏（navbar）上
+  title: 'Fun UI',
+
+  // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中
+  description: '一个基于 Vue 2.0 的 UI 组件库',
+
+  // 网站部署到 https://hehe1111.github.io/fun-ui/
+  base: '/fun-ui/',
+
+  // 配置默认主题
+  themeConfig: {
+    // 导航栏配置
+    nav: [
+      { text: '文档', link: '/introduction' }, // 前缀 https://hehe1111.github.io/fun-ui/
+      { text: 'GitHub', link: 'https://github.com/hehe1111/fun-ui' },
+    ],
+
+    // 侧边栏配置
+    // 可以省略 .md 拓展名，同时以 / 结尾的路径将会被视为 */README.md
+    // 如果你想要显示地指定链接的文字，使用一个格式为 [link, text] 的数组
+    // sidebar: ['/', '/install/', ['/components/', '组件']],
+
+    // 嵌套的标题链接
+    // 0 将会禁用标题（headers）链接
+    // 默认的深度是 1，它将提取到 h2 的标题
+    // 最大的深度为 2，它将同时提取 h2 和 h3 标题
+    sidebarDepth: 2,
+
+    // 侧边栏分组
+    // 侧边栏的每个子组默认是可折叠的
+    // 可以设置 collapsable: false 来让一个组永远都是展开状态。
+    sidebar: [
+      // / 基于 /docs/，而非 /docs/.vuepress/
+      '/introduction',
+      '/install',
+      '/get-started',
+      {
+        title: '组件',
+        // collapsable: false,
+        children: [
+          '/components/button',
+          '/components/collapse',
+          '/components/grid',
+          '/components/icon',
+          '/components/input',
+          '/components/layout',
+          '/components/popover',
+          '/components/tab',
+          '/components/toast',
+        ],
+      },
+    ],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+# heroImage: /hero.png
+actionText: 快速上手 →
+# 前缀 https://hehe1111.github.io/fun-ui
+actionLink: /get-started/
+features:
+  - title: 基于 Vue
+    details: 个人学习实践作品
+  - title: 参考自当下成熟的各 UI 库
+    details: Framework7、Ant Design of React、Element UI、Mint-UI、iView
+footer: MIT Licensed | Copyright © 2019-present hehe1111
+---
+
+> **警告：该 UI 组件库是个人尝试造轮子的作品，请不要应用于生产环境！**
