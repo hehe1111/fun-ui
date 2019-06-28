bf2e2658000aa51633a249e93db3ed05b05e71fb
hehe1111 <2908749709@qq.com>
2019-06-28 18:59:20 +0800

    feat: 初步确定如何进行渲染

---
+++ src/components/FCascader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="cascader">
+    <div class="trigger" @click="isPopoverVisiable = !isPopoverVisiable"></div>
+    <div class="popover" v-if="isPopoverVisiable">
+      <f-cascader-items :items="source" />
+    </div>
+  </div>
+</template>
+
+<script>
+import FCascaderItems from './FCascaderItems';
+
+export default {
+  name: 'FunUIFCascader',
+  props: {
+    source: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      isPopoverVisiable: false,
+    };
+  },
+  components: { FCascaderItems },
+};
+</script>
+<style lang="scss" scoped>
+.cascader {
+  .trigger {
+    width: 20em;
+    height: 2em;
+    border: 1px solid red;
+  }
+  .popover {
+    height: 14em;
+    overflow: auto;
+    border: 1px solid red;
+    display: flex;
+  }
+}
+</style>
---
+++ src/components/FCascaderItems.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cascader-items">
+    <div class="level left">
+      <div
+        class="label"
+        v-for="item in items"
+        :key="item.name"
+        @click="selected = item"
+      >
+        {{ item.name }}
+      </div>
+    </div>
+    <div
+      class="level right"
+      v-if="selected && selected.children && selected.children.length"
+    >
+      <fun-u-i-cascader-items :items="selected.children" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FunUICascaderItems',
+  props: {
+    items: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      selected: null,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.cascader-items {
+  height: 100%;
+  display: flex;
+  > .level {
+    height: 100%;
+  }
+  > .left {
+    border: 1px solid red;
+    > .label {
+      padding: 0.5em 1em;
+    }
+  }
+}
+</style>
