2943bd579b92ac885766e3b4425359b8f927cc72
hehe1111 <2908749709@qq.com>
2019-07-07 15:43:24 +0800

    docs: 更新 FCarousel 组件文档

---
+++ docs/.vuepress/components/demo-carousel-4.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-button-group-and-carousel">
+      <f-button-group class="demo-button-group">
+        <f-button @click="dotPosition1 = 'left'">left</f-button>
+        <f-button @click="dotPosition1 = 'right'">right</f-button>
+      </f-button-group>
+      <f-carousel class="demo-carousel" :dot-position="dotPosition1" :enable-arrow="true">
+        <f-carousel-item name="x1">
+          <div class="image">1</div>
+        </f-carousel-item>
+        <f-carousel-item name="x2">
+          <div class="image">2</div>
+        </f-carousel-item>
+        <f-carousel-item name="x3">
+          <div class="image">3</div>
+        </f-carousel-item>
+        <f-carousel-item name="x4">
+          <div class="image">4</div>
+        </f-carousel-item>
+      </f-carousel>
+    </div>
+
+    <div class="demo-button-group-and-carousel">
+      <f-button-group class="demo-button-group">
+        <f-button @click="dotPosition2 = 'top'">top</f-button>
+        <f-button @click="dotPosition2 = 'bottom'">bottom（默认值）</f-button>
+      </f-button-group>
+      <f-carousel class="demo-carousel" :dot-position="dotPosition2" :enable-arrow="true">
+        <f-carousel-item name="x1">
+          <div class="image">1</div>
+        </f-carousel-item>
+        <f-carousel-item name="x2">
+          <div class="image">2</div>
+        </f-carousel-item>
+        <f-carousel-item name="x3">
+          <div class="image">3</div>
+        </f-carousel-item>
+        <f-carousel-item name="x4">
+          <div class="image">4</div>
+        </f-carousel-item>
+      </f-carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+import FButton from "../../../src/components/FButton.vue";
+import FButtonGroup from "../../../src/components/FButtonGroup.vue";
+import FCarousel from "../../../src/components/FCarousel.vue";
+import FCarouselItem from "../../../src/components/FCarouselItem.vue";
+
+export default {
+  data() {
+    return {
+      dotPosition1: "left",
+      dotPosition2: "top",
+    };
+  },
+  components: { FButton, FButtonGroup, FCarousel, FCarouselItem },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/demo-carousel.scss";
+
+.demo-container {
+  display: flex;
+
+  > .demo-button-group-and-carousel {
+    display: flex;
+    flex-direction: column;
+    &:first-child {
+      margin-right: 2em;
+    }
+
+    > .demo-button-group {
+      margin: 1em 0;
+    }
+  }
+}
+</style>
--- docs/components/carousel.md
+++ docs/components/carousel.md
@@ -10,14 +10,36 @@
 
 - 禁止自动播放
 
+  ```vue
+  :auto-play="false"
+  ```
+
   <ClientOnly>
     <demo-carousel-2 />
   </ClientOnly>
 
 - 允许用箭头指示器切换
 
+  ```vue
+  :enable-arrow="true"
+  ```
+
   <ClientOnly>
     <demo-carousel-3 />
+  </ClientOnly>
+
+- 允许配置圆点的位置
+
+  - 可选值：`top`/`bottom`/`left`/`right`
+  - 默认值：`bottom`
+  - **注意：`left`/`right` 时 `enableArrow` 无效**
+
+  ```vue
+  dot-position="left"
+  ```
+
+  <ClientOnly>
+    <demo-carousel-4 />
   </ClientOnly>
 
 ## API
