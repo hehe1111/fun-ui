2d98d0ab08e1871bf2171ed5b82d945f12ca46b5
hehe1111 <2908749709@qq.com>
2019-07-11 20:42:32 +0800

    test: 引入 karma 做测试运行器
    vue-test-utils 在 jsdom 中进行测试，只能测试内联样式。引入 karma
    用浏览器测试，则外部样式也可以测试

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+// https://vue-test-utils.vuejs.org/zh/guides/testing-single-file-components-with-karma.html
+// https://cli.vuejs.org/zh/guide/webpack.html#以一个文件的方式使用解析好的配置
+
+var webpackConfig = require('@vue/cli-service/webpack.config.js');
+
+// eslint-disable-next-line prettier/prettier
+module.exports = function (config) {
+  config.set({
+    frameworks: ['mocha'],
+
+    files: ['tests/**/*.spec.js'],
+
+    preprocessors: {
+      '**/*.spec.js': ['webpack', 'sourcemap'],
+    },
+
+    webpack: webpackConfig,
+
+    reporters: ['spec'],
+
+    browsers: ['ChromeHeadless'],
+  });
+};
