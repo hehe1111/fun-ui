5cd7e445cfa5093739e14f1fdfc35f4037738eb6
hehe1111 <2908749709@qq.com>
2019-07-13 17:33:19 +0800

    refactor(FPagination 组件): 将重复的模板拆分成单文件组件

---
+++ src/components/pagination/FPagination.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="f-pagination">
+    <f-pagination-buttons
+      :current.sync="mutableCurrent"
+      :total="total"
+      :no-gutter="noGutter"
+      :hide-on-single-page="hideOnSinglePage"
+      :on-page-change="onPageChange"
+    />
+  </div>
+</template>
+
+<script>
+import FPaginationButtons from './FPaginationButtons.vue';
+
+export default {
+  name: 'FunUIPagination',
+  props: {
+    current: {
+      type: Number,
+      default: 1,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    noGutter: {
+      type: Boolean,
+      default: false,
+    },
+    hideOnSinglePage: {
+      type: Boolean,
+      default: false,
+    },
+    onPageChange: Function,
+  },
+  data() {
+    return {
+      mutableCurrent: null,
+    };
+  },
+  created() {
+    this.mutableCurrent = this.current;
+  },
+  watch: {
+    mutableCurrent(newValue) {
+      this.$emit('update:current', newValue);
+    },
+  },
+  components: { FPaginationButtons },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/pagination/FPaginationButtons.vue
@@ -0,0 +1,142 @@
+<template>
+  <div
+    class="f-pagination-buttons"
+    :class="buttonGroupClass"
+    v-if="hideIfSinglePage"
+  >
+    <f-button icon="left" :class="disabledClassLeft" @click="goToPrevious" />
+    <f-button
+      v-for="(n, index) in pageNumberArray"
+      :key="index"
+      @click="goToPage(n)"
+      :class="classes(n)"
+      >{{ n }}</f-button
+    >
+    <f-button icon="right" :class="disabledClassRight" @click="goToNext" />
+  </div>
+</template>
+
+<script>
+import FButton from '../button/FButton.vue';
+
+const separator = '...';
+
+export default {
+  name: 'FunUIPagination',
+  props: {
+    current: {
+      type: Number,
+      default: 1,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    noGutter: {
+      type: Boolean,
+      default: false,
+    },
+    hideOnSinglePage: {
+      type: Boolean,
+      default: false,
+    },
+    onPageChange: Function,
+  },
+  computed: {
+    buttonGroupClass() {
+      return {
+        'f-button-group': this.noGutter,
+      };
+    },
+    pageNumberArray() {
+      return this.getPageNumberArray(this.current, this.total);
+    },
+    disabledClassLeft() {
+      return { disabled: this.current === 1 };
+    },
+    disabledClassRight() {
+      return { disabled: this.current === this.total };
+    },
+    hideIfSinglePage() {
+      if (this.pageNumberArray.length > 1) return true;
+      return this.pageNumberArray.length === 1 && !this.hideOnSinglePage;
+    },
+  },
+  methods: {
+    getPageNumberArray(currentPage, totalPages) {
+      if (
+        typeof currentPage !== 'number' ||
+        typeof totalPages !== 'number' ||
+        currentPage <= 0 ||
+        totalPages <= 0 ||
+        currentPage > totalPages
+      ) {
+        return [];
+      }
+      const result = [
+        currentPage - 2,
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        currentPage + 2,
+      ].filter(value => value > 0 && value <= totalPages);
+      currentPage - 3 > 1 && result.unshift(separator);
+      currentPage + 3 < totalPages && result.push(separator);
+      result.indexOf(1) === -1 && result.unshift(1);
+      result.indexOf(totalPages) === -1 && result.push(totalPages);
+      return result;
+    },
+    goToPrevious() {
+      this.current > 1 && this.onCurrentPageChange(this.current - 1);
+    },
+    goToPage(n) {
+      if (!Number.isNaN(window.parseInt(n, 10)) && n !== this.current) {
+        this.onCurrentPageChange(n);
+      }
+    },
+    goToNext() {
+      this.current < this.total && this.onCurrentPageChange(this.current + 1);
+    },
+    onCurrentPageChange(pageNumber) {
+      this.$emit('update:current', pageNumber);
+      this.onPageChange && this.onPageChange(pageNumber);
+    },
+    classes(n) {
+      return {
+        'high-light': this.current === n,
+        separator: n === separator,
+      };
+    },
+  },
+  components: { FButton },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/_var.scss';
+.f-pagination-buttons {
+  &.f-button-group {
+    @extend .f-button-group-common;
+  }
+
+  &:not(.f-button-group) > .f-button {
+    margin: 0 0.4em;
+  }
+
+  .high-light,
+  .high-light:hover,
+  .high-light:focus {
+    color: $blue;
+    border-color: $blue;
+    z-index: 1;
+  }
+
+  .high-light,
+  .high-light:hover,
+  .high-light:focus,
+  .separator {
+    background-color: #fff;
+    cursor: not-allowed;
+  }
+}
+</style>
