b3bfe42fdb40632a36746cacabb71c1b5626199f
hehe1111 <2908749709@qq.com>
2019-07-16 11:06:11 +0800

    feat(FTable.vue): 添加单选功能

--- src/components/FTable.vue
+++ src/components/FTable.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="f-table-container">
-  <table class="f-table">
-    <thead>
-      <tr>
-        <th v-if="isIdVisiable">#</th>
-        <th v-for="column in columns" :key="column.field">{{ column.text }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in dataSource" :key="item.id">
-        <td v-if="isIdVisiable">{{ item.id }}</td>
-        <td v-for="column in columns" :key="column.field">
-          {{ item[column.field] }}
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    <table class="f-table">
+      <thead>
+        <tr>
+          <th v-if="isIdVisiable">#</th>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th v-for="column in columns" :key="column.field">
+            {{ column.text }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in dataSource"
+          :key="item.id"
+          :class="highlightClass(item)"
+        >
+          <td v-if="isIdVisiable">{{ item.id }}</td>
+          <td>
+            <input
+              type="checkbox"
+              :checked="getItemCheckState(item)"
+              @change="selectSingleItem(item, $event)"
+            />
+          </td>
+          <td v-for="column in columns" :key="column.field">
+            {{ item[column.field] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -32,6 +48,41 @@ export default {
     isIdVisiable: {
       type: Boolean,
       default: false,
+    },
+    selectedItems: {
+      type: Array,
+      default: () => [],
+      validator: array => {
+        return array.every(n => typeof n === 'number');
+      },
+    },
+  },
+  data() {
+    return {
+      mutableSelectedItems: [],
+    };
+  },
+  created() {
+    this.mutableSelectedItems = [...this.selectedItems];
+  },
+  methods: {
+    highlightClass(item) {
+      return { highlight: this.mutableSelectedItems.indexOf(item.id) >= 0 };
+    },
+    selectSingleItem({ id }, $event) {
+      if ($event.target.checked) {
+        this.mutableSelectedItems.push(id);
+      } else {
+        const index = this.mutableSelectedItems.indexOf(id);
+        index >= 0 && this.mutableSelectedItems.splice(index, 1);
+      }
+    },
+    getItemCheckState(item) {
+      return this.mutableSelectedItems.indexOf(item.id) >= 0;
+    },
+  watch: {
+    mutableSelectedItems(newValue, oldValue) {
+      this.$emit('update:selectedItems', newValue);
     },
   },
 };
@@ -67,6 +118,15 @@ export default {
 
         &:hover {
           background-color: $greyHover;
+        }
+
+        &.highlight {
+          background-color: lighten($blue, 10%);
+          color: #fff;
+
+          > td {
+            border-bottom-color: #fff;
+          }
         }
       }
     }
