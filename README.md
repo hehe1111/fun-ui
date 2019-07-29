f7e64542bef9cae08970f64389b6e0b8e87baa28
hehe1111 <2908749709@qq.com>
2019-07-29 17:58:31 +0800

    打包发布到 npm

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # fun-ui
+
+> A UI toolkit base on Vue 2.6.
+
+## Install
+
+```shell
+npm install fun-ui
+
+# or
+
+yarn add fun-ui
+```
+
+## Quick Start
+
+```vue
+<template>
+  <div id="app">
+    <f-button>Click me</f-button>
+  </div>
+</template>
+
+<script>
+import { FButton } from "fun-ui";
+import "fun-ui/dist/fun-ui.css";
+
+export default {
+  name: "app",
+  components: { FButton },
+};
+</script>
+```
+
+## LICENSE
+
+MIT.
---
+++ src/index.js
@@ -0,0 +1,43 @@
+export { default as FIcon } from './components/FIcon.vue';
+export { default as FButton } from './components/button/FButton.vue';
+export { default as FButtonGroup } from './components/button/FButtonGroup.vue';
+export { default as FCarousel } from './components/carousel/FCarousel.vue';
+export {
+  default as FCarouselItem,
+} from './components/carousel/FCarouselItem.vue';
+export { default as FCascader } from './components/cascader/FCascader.vue';
+export {
+  default as FCascaderItems,
+} from './components/cascader/FCascaderItems.vue';
+export { default as FCollapse } from './components/collapse/FCollapse.vue';
+export {
+  default as FCollapseItem,
+} from './components/collapse/FCollapseItem.vue';
+export { default as FCol } from './components/grid/FCol.vue';
+export { default as FRow } from './components/grid/FRow.vue';
+export { default as FLayout } from './components/layout/FLayout.vue';
+export { default as FHeader } from './components/layout/FHeader.vue';
+export { default as FContent } from './components/layout/FContent.vue';
+export { default as FSidebar } from './components/layout/FSidebar.vue';
+export { default as FFooter } from './components/layout/FFooter.vue';
+export { default as FNav } from './components/nav/FNav.vue';
+export { default as FSubNav } from './components/nav/FSubNav.vue';
+export { default as FNavItem } from './components/nav/FNavItem.vue';
+export {
+  default as FPagination,
+} from './components/pagination/FPagination.vue';
+export {
+  default as FPaginationButtons,
+} from './components/pagination/FPaginationButtons.vue';
+export { default as FTab } from './components/tab/FTab.vue';
+export { default as FTabBody } from './components/tab/FTabBody.vue';
+export { default as FTabBodyItem } from './components/tab/FTabBodyItem.vue';
+export { default as FTabNav } from './components/tab/FTabNav.vue';
+export { default as FTabNavItem } from './components/tab/FTabNavItem.vue';
+export { default as FInput } from './components/FInput.vue';
+export { default as FPopover } from './components/FPopover.vue';
+export { default as FSticky } from './components/FSticky.vue';
+export { default as FTable } from './components/FTable.vue';
+export { default as FToast } from './plugins/toast.js';
+export { default as FUpload } from './components/FUpload.vue';
+export { default as clickOutside } from './directives/click-outside.js';
