6152fd19dd864c61e6e856f8d339ac5f91a78603
hehe1111 <2908749709@qq.com>
2019-08-12 22:06:34 +0800

    feat(FButton.vue): 添加 type  round  small 属性

--- src/components/button/FButton.vue
+++ src/components/button/FButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="f-button" :class="{ disabled }" @click="onClick">
+  <button :class="classes" @click="onClick">
     <f-icon
       v-if="icon"
       :name="icon"
@@ -11,9 +11,11 @@
 
 <script>
 import FIcon from '../FIcon.vue';
+import { optionsName2ClassPrefix, oneOf } from '../../assets/utils.js';
 
 export default {
   name: 'FunUIButton',
+  components: { FIcon },
   props: {
     icon: {
       type: String,
@@ -25,9 +27,48 @@ export default {
         return value === 'left' || value === 'right';
       },
     },
+    type: {
+      type: String,
+      default: 'default',
+      validator(prop) {
+        return oneOf(prop, [
+          'default',
+          'primary',
+          'info',
+          'success',
+          'warning',
+          'danger',
+        ]);
+      },
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    small: {
+      type: Boolean,
+      default: false,
+    },
     disabled: {
       type: Boolean,
       default: false,
+    },
+  },
+  computed: {
+    n2c() {
+      return optionsName2ClassPrefix(this.$options.name);
+    },
+    classes() {
+      const { n2c, type, round, small, disabled } = this;
+      return [
+        n2c(),
+        {
+          [`${n2c(type)}`]: type,
+          [`${n2c('round')}`]: round,
+          [`${n2c('small')}`]: small,
+          [`${n2c('disabled')}`]: disabled,
+        },
+      ];
     },
   },
   methods: {
@@ -35,47 +76,103 @@ export default {
       !this.disabled && this.$emit('click', $event);
     },
   },
-  components: { FIcon },
 };
 </script>
 
 <style lang="scss" scoped>
 @import '../../assets/_var.scss';
 
+@mixin button-color($color) {
+  background-color: $color;
+  border-color: $color;
+  &:hover,
+  &:focus {
+    background-color: lighten($color, 10%);
+    border-color: lighten($color, 10%);
+  }
+  &:active {
+    background-color: $color;
+    border-color: $color;
+  }
+}
+
 .f-button {
   @extend .inline-flex-center;
-  height: $height;
   line-height: 1;
   font-size: $fontSize;
-  padding: 0 1em;
+  padding: 0.5em 1em;
+  margin: 4px;
   border-radius: $borderRadius;
-  border: 1px solid $borderColor;
-  background-color: $buttonBgColor;
+  border: 1px solid transparent;
   user-select: none;
   cursor: pointer;
-  @extend .inline-flex-center;
+  transition: all $duration linear;
   // top/middle/bottom/text-top/text-bottom/-webkit-baseline-middle 都可以解决 inline 对不齐的 bug
   vertical-align: middle;
   white-space: nowrap;
 
-  &:hover {
-    border-color: $borderColorHover;
-  }
-  &:active {
-    background-color: $buttonActiveBgColor;
-  }
   &:focus {
     outline: none;
   }
-  &.disabled {
+  &:active {
+    transform: scale(0.9);
+  }
+
+  &-default {
+    background-color: $buttonBgColor;
+    border-color: $borderColor;
+    &:hover {
+      border-color: $borderColorHover;
+    }
+    &:active {
+      background-color: lighten($grey, 20%);
+    }
+  }
+
+  &-primary,
+  &-info,
+  &-success,
+  &-warning,
+  &-danger {
+    color: #fff;
+  }
+  &-primary {
+    @include button-color($blue);
+  }
+  &-info {
+    @include button-color($grey);
+  }
+  &-success {
+    @include button-color($green);
+  }
+  &-warning {
+    @include button-color($yellow);
+  }
+  &-danger {
+    @include button-color($red);
+  }
+
+  &-round {
+    border-radius: 1000px;
+  }
+
+  &-small {
+    padding: 4px;
+  }
+
+  &-disabled {
     cursor: not-allowed;
-    background-color: $grey;
-    color: $disabledGrey;
+    color: #fff;
+    &,
+    &:hover,
+    &:focus,
+    &:active {
+      transform: scale(1);
+      background-color: lighten($grey, 30%);
+      border-color: lighten($grey, 30%);
+    }
   }
-  &.disabled,
-  &.disabled:hover {
-    border-color: $disabledGrey;
-  }
+
   > .icon-left {
     margin-right: 6px;
   }
