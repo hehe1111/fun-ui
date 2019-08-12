41519854b4abf3a4c50eb435cdee93dbdbdaa83f
hehe1111 <2908749709@qq.com>
2019-08-12 20:10:44 +0800

    feat(FIcon.vue): 添加 size 和 color 属性；规范组件 CSS 类名

--- src/components/FIcon.vue
+++ src/components/FIcon.vue
@@ -1,43 +1,58 @@
 <template>
-  <svg
-    class="icon"
-    :class="classes"
-    aria-hidden="true"
-    @click="$emit('click', $event)"
-  >
+  <svg :class="classes" @click="$emit('click', $event)" ref="svgRef">
     <use :xlink:href="`#icon-${name}`" />
   </svg>
 </template>
 
 <script>
 import '../assets/svg.js';
+import { optionsName2ClassPrefix } from '../assets/utils.js';
 
 export default {
   name: 'FunUIIcon',
   props: {
     name: {
       type: String,
-      validator(value) {
-        return !!value;
+      required: true,
+      validator(prop) {
+        return !!prop;
       },
+    },
+    size: {
+      type: String,
+    },
+    color: {
+      type: String,
     },
   },
   computed: {
+    n2c() {
+      return optionsName2ClassPrefix(this.$options.name);
+    },
     classes() {
-      return this.name === 'loading' ? 'loading' : '';
+      const { n2c } = this;
+      return [`${n2c()}`, this.name === 'loading' ? n2c('loading') : ''];
+    },
+  },
+  mounted() {
+    this.updateStyle();
+  },
+  methods: {
+    updateStyle() {
+      const { style } = this.$refs.svgRef;
+      const { size, color } = this;
+      if (size) {
+        style.width = `${size}`;
+        style.height = `${size}`;
+      }
+      color && (style.fill = `${color}`);
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
+@import '../assets/_var.scss';
 
 @keyframes spin {
   0% {
@@ -48,7 +63,13 @@ export default {
   }
 }
 
-.loading {
-  animation: spin 1s infinite linear;
+.f-icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+
+  &-loading {
+    animation: spin 1s infinite linear;
+  }
 }
 </style>
