e33f732b74ac2a3b57072f60ea812b6c801a003a
hehe1111 <2908749709@qq.com>
2019-08-15 21:57:15 +0800

    feat(FSidebar.vue): 添加 switchable  position 属性
    sidebar 位于右边并且 switchable 为 true，并且 position 为 right
    时，过渡有问题，待解决

--- src/components/layout/FSidebar.vue
+++ src/components/layout/FSidebar.vue
@@ -1,22 +1,23 @@
 <template>
   <div :class="n2c('container')">
-    <transition name="sidebar-slide">
-      <div v-if="isSwitchOn">
+    <transition :name="`sidebar-${position}-slide`">
+      <div :class="n2c('sidebar')" v-if="isSwitchOn">
         <slot />
       </div>
     </transition>
-    <div :class="n2c('switch')" @click="toggleSwitch">
-      <f-icon
-        name="right"
-        :class="n2c('switch-icon', isSwitchOn ? 'switch-icon-left' : '')"
-      />
+    <div
+      v-if="switchable"
+      :class="n2c('switch', `switch-${position}`)"
+      @click="toggleSwitch"
+    >
+      <f-icon :name="iconName" :class="iconClasses" />
     </div>
   </div>
 </template>
 
 <script>
 import FIcon from '../FIcon.vue';
-import { optionsName2ClassPrefix } from '../../assets/utils.js';
+import { optionsName2ClassPrefix, oneOf } from '../../assets/utils.js';
 
 export default {
   name: 'FunUISidebar',
@@ -26,9 +27,30 @@ export default {
       isSwitchOn: true,
     };
   },
+  props: {
+    switchable: {
+      type: Boolean,
+      default: false,
+    },
+    position: {
+      type: String,
+      default: 'left',
+      validator(prop) {
+        return oneOf(prop, ['left', 'right']);
+      },
+    },
+  },
   computed: {
     n2c() {
       return optionsName2ClassPrefix(this.$options.name);
+    },
+    iconName() {
+      return this.position === 'left' ? 'right' : 'left';
+    },
+    iconClasses() {
+      return [
+        this.n2c('switch-icon', this.isSwitchOn ? 'switch-icon-reversed' : ''),
+      ];
     },
   },
   methods: {
@@ -42,17 +64,34 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/_var.scss';
 
-.sidebar-slide-enter-active {
-  /* 0 到 100% */
-  animation: sidebar-slide $duration;
+.sidebar-left-slide-enter-active {
+  /* 0 to 100% */
+  animation: sidebar-left-slide $duration;
 }
-.sidebar-slide-leave-active {
-  /* 100% 到 0 */
-  animation: sidebar-slide $duration reverse;
+.sidebar-left-slide-leave-active {
+  /* 100% to 0 */
+  animation: sidebar-left-slide $duration reverse;
 }
-@keyframes sidebar-slide {
+@keyframes sidebar-left-slide {
   0% {
     margin-left: -100%;
+  }
+  100% {
+    margin-left: 0;
+  }
+}
+
+.sidebar-right-slide-enter-active {
+  /* 0 to 100% */
+  animation: sidebar-right-slide $duration;
+}
+.sidebar-right-slide-leave-active {
+  /* 100% to 0 */
+  animation: sidebar-right-slide $duration reverse;
+}
+@keyframes sidebar-right-slide {
+  0% {
+    margin-left: 100%;
   }
   100% {
     margin-left: 0;
@@ -74,14 +113,21 @@ export default {
     word-break: keep-all;
     position: absolute;
     top: 0;
-    right: 0;
+
+    &-left {
+      right: 0;
+    }
+
+    &-right {
+      left: 0;
+    }
 
     &-icon {
       margin: 0 0.5em;
       white-space: nowrap;
       transition: transform $duration linear;
 
-      &-left {
+      &-reversed {
         transform: rotate(180deg);
       }
     }
