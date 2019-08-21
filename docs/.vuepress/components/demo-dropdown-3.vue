9877069372bbe23fa597daf9c112e1e9f94da59e
hehe1111 <2908749709@qq.com>
2019-08-21 11:11:05 +0800

    docs(dropdown): update docs

---
+++ docs/.vuepress/components/demo-dropdown-3.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="demo-dropdown-3 extra-margin">
+    <f-dropdown>
+      <template slot="dropdown">
+        <f-dropdown-item
+          v-for="(n, index) in dataSource"
+          :key="n"
+          class="item"
+          :disabled="index === 1"
+        >
+          <span>{{ n }}</span>
+        </f-dropdown-item>
+      </template>
+      <f-button>
+        Dropdown
+        <f-icon name="down" />
+      </f-button>
+    </f-dropdown>
+
+    <code-box :htmlCode="htmlCode" :javascriptCode="javascriptCode" />
+  </div>
+</template>
+
+<script>
+import FDropdown from '../../../src/components/dropdown/FDropdown.vue';
+import FDropdownItem from '../../../src/components/dropdown/FDropdownItem.vue';
+import FButton from '../../../src/components/button/FButton.vue';
+import FIcon from '../../../src/components/FIcon.vue';
+
+export default {
+  name: 'demo-dropdown-3',
+  components: { FDropdown, FDropdownItem, FButton, FIcon },
+  data() {
+    return {
+      dataSource: ['海贼王', '进击的巨人', '我的英雄学院', '火影忍者', '死神'],
+
+      htmlCode: `
+        <f-dropdown>
+          <template slot="dropdown">
+            <f-dropdown-item
+              v-for="(n, index) in dataSource"
+              :key="n"
+              class="item"
+              :disabled="index === 1"
+            >
+              <span>{{ n }}</span>
+            </f-dropdown-item>
+          </template>
+          <f-button>
+            Dropdown
+            <f-icon name="down" />
+          </f-button>
+        </f-dropdown>
+      `,
+      javascriptCode: `
+        data() {
+          return {
+            dataSource: ['海贼王', '进击的巨人', '我的英雄学院', '火影忍者', '死神'],
+          };
+        },
+      `,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/_var.scss';
+@import '../assets/style/demo-dropdown.scss';
+</style>
